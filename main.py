acd8eeb3b06588837d693fe4b518d5045f750b9c
Saurabh Nanda <83748468+SN-18@users.noreply.github.com>
2023-09-28 20:37:25 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+@authors: Firodi, Nanda, Shenker-Tauris
+@Github: abhishgain99, SN-18, lorem_ipsum
+@file: main.py
+@return: returns graph topologies that are location-optimized and plots these optimizations
+@see: README.md at https://github.com/SN-18/CSC-555-Project-asfirodi-snanda2-mshenke
+
+#############################################################################################################################################################
+'''
+
+
+import argparse
+import dimod
+import sys
+import networkx as nx
+import numpy as np
+import matplotlib as plt
+
+
+
+
