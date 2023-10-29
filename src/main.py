cdc001c65d5bb1066153de9aa0f779e413447727
Saurabh Nanda <83748468+SN-18@users.noreply.github.com>
2023-10-29 08:37:48 -0400

    Update main.py
    Add Distance Calculations

--- src/main.py
+++ src/main.py
@@ -9,17 +9,20 @@
 #############################################################################################################################################################
 '''
 
+# import subprocess
 
 # installs
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+# def install(package):
+#     # subprocess.run(['pip', 'install', '-r', 'requirements.txt']
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 #Installs
-install("dimod")
-install("pandas")
-install("subprocess")
+# install("dimod")
+# install("pandas")
+# install("subprocess")
 
+import numpy as np
 
 if __name__ == "__main__":
 
@@ -39,6 +42,136 @@ if __name__ == "__main__":
 
     df = pd.read_csv(file_path)
     print(df.head())
+
+
+'''
+     ###################################################################################################
+    What I'm trying to do:
+    1. Take the bike-share database
+    2. Construct Distance matrix
+    3. Use BQM on distance Matrix
+    4. By iterating over each node, and it's neighbour
+    5. Find the highest energy, report most optimal row(s)
+    6. Repeat over several runs by eliminating visited rows, to know resource allocation in descending order
+     #################################################################################################################
+     
+     
+     
+                                            DISTANCE MATRIX CALCULATIONS CODE:
+    #################################################################################################################
+    
+'''
+
+import pandas
+import math
+import dimod
+
+#Calculate Euclidean Distance between two bike-sharing nodes
+def euclidean(node1,node2):
+	d = 0
+	for i in range(len(node1)):
+		num1 = node1[i]
+		num2 = node2[i]
+		d = d + math.sqrt(abs((num1)**2 - (num2)**2))
+	return d
+
+# Reading the CSV file, splitting into even columns
+df = pandas.read_csv('daily-bike-share.csv')
+print(df.head())
+l = len(df)
+first_frame = df[:l//2]
+second_frame = df[l//2:]
+
+#Construct a (n/2)*(n/2) distance matrix full of zeros
+distance_matrix = []
+for i in range(l//2):
+	small_arr = []
+	for j in range(l//2):
+		small_arr.append(0)
+	distance_matrix.append(small_arr)
+
+
+
+
+#CREATING DISTANCE MATRIX
+i=j=0
+for index, row in first_frame.iterrows():
+	for index2,row2 in second_frame.iterrows():
+		if j>=l or i>=l:
+			break
+		distance_matrix[i][j] = euclidean(row, row2)
+		print(distance_matrix[i][j])
+		j = j + 1
+	i = i + 1
+	if i>=l:
+		break
+
+#Debug
+    # print(row['c1'], row['c2'])
+# for i in range(1,len(first_frame)):
+# 	for j in range(1,len(second_frame)):
+# 		print(first_frame[i],second_frame[j])
+# 		distance_matrix.append[i][j] = euclidean(first_frame[i],second_frame[j])
+#End of Debug
+
+print("Distance Matrix is: ")
+print(distance_matrix)
+
+
+#Now trying out bqm on distance matrix
+bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
+
+node_dict = []
+
+for index, row in df.iterrows():
+	node = []
+	for element in row:
+		node.append(element)
+	node_dict.append(node)
+
+print(node_dict)
+
+
+distance_dict = [0]
+for i in range(2,len(node_dict)):
+	print(node_dict[i])
+	distance = euclidean(node_dict[i],node_dict[0])
+	distance_dict.append(distance)
+
+print(distance_dict)
+
+for node, in range(len(distance_matrix)):
+    for neighbor in range(node):
+        # node_dict.items():
+	# Define a variable for the node
+        v_node = f'node_{node}'
+        bqm.add_variable(v_node, 1.0)
+
+	    # for neighbor in neighbors:
+		# Define a variable for the neighbor
+		v_neighbor = f'node_{neighbor}'
+
+		# Assuming you want to add an interaction based on the difference between node and neighbor
+		# Adjust this interaction based on your specific problem
+		interaction = (node - neighbor) // node
+
+		if interaction != 0:
+		    try:
+				bqm.add_interaction(v_node, v_neighbor, interaction)
+			except:
+				pass
+
+
+
+
+
+'''                                               Distance matrix calculations code end
+################################################################################################################################################################
+'''
+
+
+
+
 
     #debugging for a different xlrd engine
     # if file.endswith('.xlsx'):
@@ -110,8 +243,8 @@ if __name__ == "__main__":
     # bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
 
     #End of Debug
-
-
+    # quadratic = (np.arange(0, len(df.day)), np.arange(1, len(df.day)), -np.ones(len(df.day)))
+    # bqm = dimod.BQM.from_numpy_vectors(len(df.day)*len(df.day), quadratic, 0, "BINARY")
 
     bqm = dimod.BinaryQuadraticModel(len(df.day)*len(df.day),'BINARY')
 
@@ -127,9 +260,10 @@ if __name__ == "__main__":
             # Assuming you want to add an interaction based on the difference between node and neighbor
             # Adjust this interaction based on your specific problem
             interaction = (node - neighbor)//node
+
             if interaction!=0:
                 try:
-                    bqm.add_interaction(v_node, v_neighbor, interaction)
+                    bqm.add_interaction(v_node, v_neighbor,interaction)
                 except:
                     pass
 
@@ -150,9 +284,12 @@ if __name__ == "__main__":
         #End of Debug
 
 
-
     sampler = dimod.ExactSolver()
-    response = sampler.sample(bqm)
+    l=len(bqm)
+    print("l is:",l)
+    num = 5
+    n_a=np.array(bqm)
+    response = sampler.sample(n_a.reshape(1,-1))
 
 
     #Debug
